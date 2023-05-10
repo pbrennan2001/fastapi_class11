4ce8c0ffc024fbdb740faaada08303d876c295e6
pbrennan2001 <patrick.brennan.1@stonybrook.edu>
2023-05-10 00:20:42 +0000

    changed flask format to fastapi

--- main.py
+++ main.py
@@ -5,29 +5,30 @@ import pandas as pd
 
 df = pd.read_csv('./data/Drugs2018.csv')
 
-app = FastAPI(debug=True)
+app = FastAPI()
 
-@app.route('/', methods=["GET"])
+
+@app.get('/')
 def home():
     return 'this is an API service for NPR code details'
 
-@app.route('/preview', methods=["GET"])
+@app.get('/preview')
 def preview():
     top10rows = df.head(1)
     result = top10rows.to_json(orient="records")
     return result
 
-@app.route('/icd/<value>', methods=['GET'])
-def icdcode(value):
+@app.get('/npr/{value}')
+def nprcode(value: str):
     print('value: ', value)
-    filtered = df[df['principal_diagnosis_code'] == value]
+    filtered = df[df['NPROPNAME'] == value]
     if len(filtered) <= 0:
         return 'Not Available'
     else: 
         return filtered.to_json(orient="records")
 
-@app.route('/icd/<value>/sex/<value2>')
-def icdcode2(value, value2):
+@app.get('/icd/{value}/sex/{value2}')
+def icdcode2(value: str, value2: str):
     filtered = df[df['principal_diagnosis_code'] == value]
     filtered2 = filtered[filtered['sex'] == value2]
     if len(filtered2) <= 0:
